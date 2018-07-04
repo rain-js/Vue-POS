fa86c3049ad9564661444c13739215e09fb5dc86
rain-js <unique_ritchie@163.com>
2018-07-05 00:30:56 +0800

    add Pos.vue

---
+++ src/components/Pos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="pos">{{ msg }}</div>
+</template>
+
+<script>
+    export default {
+        name: 'Pos',
+        data () {
+            return {
+                msg: 'Hello Pos Demo'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
