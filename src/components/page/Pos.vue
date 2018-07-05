dacf379a957e4af683d415738bb04ee8229bbfd7
rain-js <unique_ritchie@163.com>
2018-07-05 13:35:51 +0800

    :sparkles:引入 element-ui

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="pos">{{ msg }}</div>
+    <div class="pos">
+        <el-row>
+            <el-col :span="6" class="order-list fix-height">
+                <div>订单栏</div>
+            </el-col>
+            <el-col :span="18" class="goods-list fix-height">
+                <div>产品栏</div>
+            </el-col>
+        </el-row>
+    </div>
+    
 </template>
 
 <script>
@@ -9,13 +19,29 @@
             return {
                 msg: 'Hello Pos Demo'
             }
+        },
+        mounted() {
+            // height: 100%
+            let ch = document.body.clientHeight,
+                x = document.getElementsByClassName('fix-height'),
+                aX = Array.from(x)
+            
+            aX.forEach(element => {
+                element.style.height =  ch + 'px'      
+            })
         }
     }
 </script>
 
 <style scoped>
-.pos {
-    overflow: hidden;
-    border: 2px solid salmon;
-}
+    .pos {
+        overflow: hidden;
+    }
+    .order-list {
+        border-right: 1px solid blue;
+        background: #d3dce6;
+    }
+    .goods-list {
+        background: #e5e9f2;
+    }
 </style>
